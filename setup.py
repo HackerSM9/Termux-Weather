b821859e2e4cc1c4cbdefa8cf8e538c36a2a16aa
Samarth Magdum (SM9) <101000624+Hackersm9@users.noreply.github.com>
2022-07-03 13:34:24 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+import os,sys
+
+os.system("clear")
+os.system("pkg install git python3 -y ")
+os.system("cd && git clone https://github.com/Hackersm9/Termux-Weather")
+os.system("clear")
+os.system("cd Termux-Weather")
+os.system("clear")
