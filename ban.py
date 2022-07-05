f4252b178f3d8804aaff01ad03c955413a9db3f0
Samarth Magdum (SM9) <101000624+Hackersm9@users.noreply.github.com>
2022-07-05 13:56:51 +0530

    Update ban.py

--- ban.py
+++ ban.py
@@ -3,9 +3,9 @@ import os,sys
 cast = int(input("Enter your Choice: "))
 if (cast == 1):
     os.system("python3 weather.py")
-    if (cast == 2):
+if (cast == 2):
      os.system("python3 weather2.py")
 if (cast == 3):
 	print("Option 3 is in not Available Yet")
 else:
-    print("Processing...")
+    print("Done !")
