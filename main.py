8f183b8385de84423c82efbc43a4e39c6cc2737b
Samarth Magdum (SM9) <101000624+Hackersm9@users.noreply.github.com>
2022-07-06 14:42:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ print(
 ██║░░░░░╚█████╔╝██║░░██║███████╗╚█████╔╝██║░░██║██████╔╝░░░██║░░░
 ╚═╝░░░░░░╚════╝░╚═╝░░╚═╝╚══════╝░╚════╝░╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░\033[0m
 ''')
-print("\033[1;32m1) Weather in column\n2) Weather in Bar-Graph\n3) Weather in State (Coming Soon)\033[0m")
-cast = int(input("\033[1;36mEnter your Choice: \033[0m"))
+print("\033[1;32m1) Weather in column\n2) Weather in Bar-Graph\n3) Weather in State (Coming Soon)\n\033[0m")
+cast = int(input("\033[1;36m\nEnter your Choice: \033[0m"))
 if (cast == 1):
     os.system("python3 .weather.py")
 if (cast == 2):
