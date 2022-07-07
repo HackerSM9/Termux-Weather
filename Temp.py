4015a6571bae1ca7e406c36f72b1eb8f3872ae5f
Samarth Magdum (SM9) <101000624+Hackersm9@users.noreply.github.com>
2022-07-07 16:32:12 +0530

    Update Temp.py

--- Temp.py
+++ Temp.py
@@ -28,7 +28,7 @@ if (cast == 4):
      os.system("python3 about.py")
 if (cast == 5):
      os.system("bash update")
-if (cast == 00)
+if (cast == 00):
      os.system("clear && python3 t.py")
 else:
      print("\033[1;31mERROR Ó╭╮Ò\033[0m")
