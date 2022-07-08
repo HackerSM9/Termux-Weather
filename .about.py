905c17fe0aa4a50fea66687971cc9eb6c8774776
Samarth Magdum (SM9) <101000624+Hackersm9@users.noreply.github.com>
2022-07-08 14:14:28 +0530

    Update .about.py

--- .about.py
+++ .about.py
@@ -1,4 +1,6 @@
 import os,sys
 
 os.system("clear")
-print("")
+print("Google Developer Account:\t\t\thttps://g.dev/Hackersm9\n
+GitHub Account:\t\t\thttps://github.com/Hackersm9\n
+Website:\t\t\thttps://s3amar.mimo.run/\n")
