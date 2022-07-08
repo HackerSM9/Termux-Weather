6e401b538ee20638d457a0389884d371fe0376d7
Samarth Magdum (SM9) <101000624+Hackersm9@users.noreply.github.com>
2022-07-08 14:38:12 +0530

    Update .about.py

--- .about.py
+++ .about.py
@@ -2,7 +2,7 @@ import os,sys
 
 os.system("clear")
 os.system("toilet -F gay HackerSM9")
-print("Google Developer Account:       \033[0;96mhttps://g.dev/Hackersm9\033[0m
-print("GitHub Account:                  \033[0;96mhttps://github.com/Hackersm9/\033[0m
-print("Website:                         \033[0;96mhttps://s3amar.mimo.run/\033[0m
+print("Google Developer Account:        \033[0;96mhttps://g.dev/Hackersm9\033[0m")
+print("GitHub Account:                  \033[0;96mhttps://github.com/Hackersm9/\033[0m")
+print("Website:                         \033[0;96mhttps://s3amar.mimo.run/\033[0m")
 print("")
