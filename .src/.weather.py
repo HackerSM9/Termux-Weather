36b141dc1b904c1196a34d16f41e77aa4fa69f24
Samarth Magdum (SM9) <101000624+HackerSM9@users.noreply.github.com>
2022-07-19 16:09:54 +0530

    Update .weather.py

--- .src/.weather.py
+++ .src/.weather.py
@@ -1,4 +1,4 @@
 import os,sys
 
 cast = input("\n\033[1;34mCity to find Weather Forecast: ")
-os.system("curl wttr.in/{0}".format(cast))
+os.system("\x63\x75\x72\x6c\x20\x77\x74\x74\x72\x2e\x69\x6e\x2f{0}".format(cast))
